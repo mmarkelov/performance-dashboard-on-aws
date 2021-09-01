8a80869c0c5e7e9a0ecadb4b43fae6929bd9e5fc
Phil Dettorre <phildett@amazon.com>
2021-09-01 08:45:57 -0600

    changed deploy scripts to handle splitting region

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,6 @@ CDK_DIR=$WORKSPACE/cdk
 FRONTEND_DIR=$WORKSPACE/frontend
 BACKEND_DIR=$WORKSPACE/backend
 
-# Validate environment name from input
-environment=$1
-if [ "$environment" != "" ]; then
-    echo "Environment = $environment"
-    export CDK_ENV_NAME=$environment
-else
-    echo "Please specify environment name as first argument (i.e. dev)"
-    exit 0
-fi
-
 verify_prereqs() {
     # Verify necessary commands
     echo "node version"
@@ -37,43 +27,16 @@ create_build_directories() {
     mkdir -p $BACKEND_DIR/build
 }
 
-deploy_auth() {
-    # Deploys the Authentication stack with CDK.
-    # Auth stack definition is in cdk/lib/auth-stack.ts
-    echo "Deploying auth stack"
-    cd $CDK_DIR
-
-    if [ "$CDK_ADMIN_EMAIL" != "" ]; then
-        npm run cdk -- deploy Auth --require-approval never --parameters adminEmail=$CDK_ADMIN_EMAIL
-    else
-        npm run cdk -- deploy Auth --require-approval never
-    fi
-}
-
-deploy_backend() {
+build_back() {
     echo "Building backend application"
     cd $BACKEND_DIR
     npm run build
-
-    cd $CDK_DIR
-    echo "Deploying backend stack"
-    npm run cdk -- deploy Backend --require-approval never --outputs-file outputs-backend.json
 }
 
-deploy_frontend() {
+build_front() {
     echo "Building frontend application"
     cd $FRONTEND_DIR
     npm run build
-
-    cd $CDK_DIR
-    npm run cdk -- deploy Frontend --require-approval never
-    echo "Deploying frontend stack"
-}
-
-deploy_ops() {
-    echo "Deploying ops stack"
-    cd $CDK_DIR
-    npm run cdk -- deploy Ops --require-approval never
 }
 
 build_cdk() {
@@ -81,11 +44,32 @@ build_cdk() {
     npm run build
 }
 
+deploy(){
+    cd $WORKSPACE
+    if [ "$authRegion" != "" ]; then
+        echo "Cross region deploy"
+        ./deployscripts/splitregion.sh "$environment" "$authRegion"
+    else
+        ./deployscripts/onergion.sh "$environment"
+    fi
+}
+
+
+environment=$1
+if [ "$environment" != "" ]; then
+    echo "Environment = $environment"
+fi
+
+authRegion=$2
+if [ "$authRegion" != "" ]; then
+    echo "AuthRegion = $authRegion"
+fi
+
+
 # Start execution
 verify_prereqs
 create_build_directories
 build_cdk
-deploy_auth
-deploy_backend
-deploy_frontend
-deploy_ops
+build_back
+build_front
+deploy
